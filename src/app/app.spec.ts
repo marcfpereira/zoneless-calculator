61db226e5e67782e0609798ce51691bb10de21d9
marcfpereira <marcfernandezpereira@gmail.com>
2025-09-26 10:37:52 +0000

    4.33

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
     }).compileComponents();
+    fixture = TestBed.createComponent(App);
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
-  });
+  it('should render router-oulet', ()=>{
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
+  })
+
 });
